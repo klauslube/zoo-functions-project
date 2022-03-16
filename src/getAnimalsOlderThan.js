6d7ff1c8a88aa77dd2fd644ae472f2377f79d8c0
klauslube <klauslube93@gmail.com>
2022-03-16 18:31:22 -0300

    Req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const find = species.find((nome) => nome.name === animal);
+  const everyResident = find.residents.every((resi) => resi.age >= age);
+  // console.log(everyResident);
+  return everyResident;
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -10,5 +10,4 @@ function getSpeciesByIds(...ids) {
     ids.some((value) => animalId.id === value));
 }
 
-console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46', '0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
