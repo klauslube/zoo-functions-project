62ec2c16cef79f775a115442affc6ef165c21865
klauslube <klauslube93@gmail.com>
2022-03-16 18:58:00 -0300

    Req 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+  if (employeeName === undefined) return {};
 
+  return employees.find((fir) => fir.firstName === employeeName || fir.lastName === employeeName);
+}
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
