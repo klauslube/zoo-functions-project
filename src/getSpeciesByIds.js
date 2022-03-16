42b0d85577add6c5ede57f0461ee9d0032974184
klauslube <klauslube93@gmail.com>
2022-03-16 16:24:30 -0300

    Req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui sada
+// console.log(data.employees);
+
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  if (ids.length === 0) return [];
+  return species.filter((animalId) =>
+    ids.some((value) => animalId.id === value));
 }
 
+console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46', '0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
