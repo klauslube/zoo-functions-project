e2d9a3c81c7d6227dba70802ab6137f659fde5fd
klauslube <klauslube93@gmail.com>
2022-03-17 15:43:53 -0300

    Req 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.
+  }
 }
-
+console.log(countAnimals());
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const filter = employees.flatMap((man) => man.managers);
+  // console.log(filter);
+  return filter.some((idMan) => idMan === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managerEmployees = employees.filter((employee) => employee.managers.includes(managerId))
+    .map((names) => names.firstName.concat(` ${names.lastName}`));
+  return managerEmployees;
 }
 
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
