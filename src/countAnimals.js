1278df045eaba6b5433fd79d2085de626becfa81
klauslube <klauslube93@gmail.com>
2022-03-17 17:24:22 -0300

    Req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,8 +3,19 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (animal === undefined) {
-    return species.
+    return species.reduce((acc, item) => {
+      acc[item.name] = item.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.specie) {
+    const name = species.find((especie) => especie.name === animal.specie);
+    if (animal.sex) {
+      const sex = name.residents.filter((count) => count.sex === animal.sex);
+      return sex.length;
+    }
+    return name.residents.length;
   }
 }
-console.log(countAnimals());
+
 module.exports = countAnimals;
